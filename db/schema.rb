e59c9d3fd0bc5b2278af866a69f80cbf5465facc
mosesochacha <ochachamoses443@@gmail.com>
2023-02-26 00:15:13 +0300

    corrected data types

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_02_25_202030) do
 
+  create_table "products", force: :cascade do |t|
+    t.string "product_name"
+    t.string "product_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comments"
+    t.integer "star_rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "products_id"
+    t.integer "users_id"
+    t.index ["products_id"], name: "index_reviews_on_products_id"
+    t.index ["users_id"], name: "index_reviews_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "user_email"
+    t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["phone_number"], name: "index_users_on_phone_number"
+    t.index ["user_email"], name: "index_users_on_user_email"
+  end
+
+  add_foreign_key "reviews", "products", column: "products_id"
+  add_foreign_key "reviews", "users", column: "users_id"
 end
