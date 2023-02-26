542af4c0ec418bb8d95c5109932902a2c10841f5
mosesochacha <ochachamoses443@@gmail.com>
2023-02-26 11:18:58 +0300

    Added instances

--- app/models/products.rb
+++ app/models/products.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
+
+def self.get_reviews
+  User.all.map{  |review| review} 
 end
+
+
+end
+
--- app/models/reviews.rb
+++ app/models/reviews.rb
@@ -1,4 +1,14 @@
 class Review < ActiveRecord::Base
   belongs_to :users
   belongs_to :products
+# Returns the User instance for this Review
+  def self.user
+    User.all
+  end
+  # Returns the Product instance for this Review
+  def self.product
+    Product.all
+  end
+
+  
 end
