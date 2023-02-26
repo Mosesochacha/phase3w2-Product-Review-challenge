36c405b42b732d3bd9a425fcb2b5d2e0ea6322a8
mosesochacha <ochachamoses443@@gmail.com>
2023-02-26 16:59:23 +0300

    Complited Aggregate and Association Methods

--- app/models/reviews.rb
+++ app/models/reviews.rb
@@ -11,6 +11,9 @@ class Review < ActiveRecord::Base
   def product
     Product.find(self.product_id)
   end
+  def print_review
+    puts "Review for #{self.product.product_name} by #{self.user.user_name}: #{self.star_rating}. #{self.comments}"
+  end
 end
 
 
